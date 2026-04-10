9731c66766b6afdf09c6dde0c37eb66d45d67e03
storidz39 <abouratil39@gmail.com>
2026-04-10 03:49:50 +0100

    Add files via upload

---
+++ api/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+
+// Aiven MySQL Credentials (from previous turn)
+const pool = mysql.createPool({
+  host: "mysql-159b2565-zinou.i.aivencloud.com",
+  port: 27815,
+  user: "avnadmin",
+  password: "AVNS_I5yYt0o7M0oY7g6fWw8",
+  database: "defaultdb",
+  ssl: {
+    rejectUnauthorized: false // Required for Aiven SSL
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
